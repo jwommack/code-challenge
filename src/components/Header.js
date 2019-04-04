3962abc6540f3ea0fce05880f7645ba7cf352c5a
Jonathan Wommack <jwommack@5sun.com>
2019-04-04 12:14:16 -0700

    small spacing change

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,7 @@ const styles = {flex:{flex: 1}};
 const Header = ({ classes }) => (
     <AppBar position="static">
         <Toolbar>
-            <Typography variant="title" color="inherit">
-                DutchBrosTest
-            </Typography>
+            <Typography variant="title" color="inherit">DutchBrosTest</Typography>
             <Button color="inherit" component={Link} to="/">Home</Button>
             <Button color="inherit" component={Link} to="/mocks">Mock Data</Button>
             <div className={classes.flex} />
