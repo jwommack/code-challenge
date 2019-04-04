f7a980333ec75661a1533d9184822138f06a40e2
Jonathan Wommack <jwommack@5sun.com>
2019-04-04 12:11:25 -0700

    Placeholder home file

---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import {
+    Typography,
+} from "@material-ui/core";
+const API = "http://localhost:3001";
+export default () => (
+    <Typography variant="display1">Not much of interest here, the meat is in Mock Data or {API}</Typography>
+);
