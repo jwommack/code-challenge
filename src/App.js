2f3907230b561476065995676f4687bf6bbb78ae
Jonathan Wommack <jwommack@5sun.com>
2019-04-04 12:12:22 -0700

    small generic main App.js

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Fragment } from "react";
+import { Route } from "react-router-dom";
+import {
+  CssBaseline,
+  withStyles,
+} from "@material-ui/core";
+import "bootstrap/dist/css/bootstrap.min.css";
+// Pages
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Mocks from "./pages/Mocks";
+
+const styles = theme => ({
+  main: {
+    padding: 3 * theme.spacing.unit,
+    [theme.breakpoints.down("xs")]: {
+      padding: 2 * theme.spacing.unit,
+    },
+  },
+});
+const App = ({ classes }) => (
+    <Fragment>
+      <CssBaseline />
+      <Header />
+      <main className={classes.main}>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/mocks" component={Mocks} />
+      </main>
+    </Fragment>
+);
+
+export default  withStyles(styles)(App);
